17028984faedac914b1cc379407cf7087a12904e
Vishwas <vishwaskumar445@gmail.com>
2022-07-01 15:07:01 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:email_password_firebase/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp(
+      title: 'Flutter Demo',
+      home: LoginScreen(),
+    );
+  }
+}
